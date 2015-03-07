6bd2ce478e2c806be448b3a85eaa6dedd9c09928
Mattew <mattewphoenyx@hotmail.fr>
2015-03-07 14:28:07 +0100

    rename + toFile

--- src/recuit/main.cpp
+++ src/recuit/main.cpp
@@ -21,6 +21,7 @@ int main()
     //vrai_test();
 
     // Partie Mattew
+
     ValueData *vA = new ValueData("A", Vecteur2D(1,1)), *vB = new ValueData("B", Vecteur2D(2,2)),
             *vC = new ValueData("C", Vecteur2D(3,2)), *vD = new ValueData("D", Vecteur2D(4,1)),
             *vE = new ValueData("E", Vecteur2D(3,0));
@@ -37,29 +38,9 @@ int main()
         }
     }
 
-    cout << "titre = recuit simule" << endl;
-    cout << "legende = Résultat du recuit simule" << endl;
-    cout << "resume = résultat du recuit simule" << endl;
-    cout << "type de scene = courbes" << endl;
-    cout << "coin bas gauche de la figure sur l'écran en coordonnées monde = ( -1, -1)" << endl;
-    cout << "coin haut droit de la figure sur l'écran en coordonnées monde = ( 10, 10)" << endl;
-    cout << "nombre de points remarquables =" << graphe.nombreSommets() << endl;
-    PElement<Sommet<ValueData>> *tempsom = graphe.lSommets;
-    while (tempsom != NULL) {
-        cout << "point remarquable = 2 black (" << tempsom->valeur->valeur.position.x << "," << tempsom->valeur->valeur.position.x << ") " << tempsom->valeur->valeur.nom << endl;
-        tempsom = tempsom->suivant;
-    }
+    graphe.toFile("recuit", "Recuit Simule", "Résultat du recuit simule", "plop");
 
-    cout << "nombre de courbes = " << graphe.nombreAretes() << endl;
-    PElement<Arete<double,ValueData>> *temp = graphe.lAretes;
-    while (temp != NULL) {
-        cout << "couleur =  green" << endl;
-        cout << "nombre de points = 2" << endl;
-        cout << "( " << temp->valeur->debut->valeur.position.x << ", " << temp->valeur->debut->valeur.position.y << ")" << endl;
-        cout << "( " << temp->valeur->fin->valeur.position.x << ", " << temp->valeur->fin->valeur.position.y << ")" << endl;
-        temp = temp->suivant;
-    }
 
-    return 0;
+
 }
 
